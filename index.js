f62527ffd74fefef617af5b8ab591086d0596945
Alessio Torricelli <Alessio92845@gmail.com>
2019-04-15 09:38:58 +0200

    FUNZIONAgit add .!

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var app = express();
+var path = require('path');
+var PORT = process.env.PORT || 5000
 
-express()
-	.use(express.static(path.join(__dirname, 'public')))
-	.set('views', path.join(__dirname, 'views'))
-	.set('view engine', 'ejs')
-	.get('/', (req, res) => res.render('pages/index'))
-	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('pages/index'));
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${ PORT }`));
