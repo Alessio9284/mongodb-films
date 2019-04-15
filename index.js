2d52e81cfa6194b6f00871ba641bf821632b74b2
Alessio Torricelli <Alessio92845@gmail.com>
2019-04-15 18:10:02 +0200

    aggiunta del json nel database -> (errore truncate)

--- index.js
+++ index.js
@@ -1,65 +1,94 @@
+/* librerie */
 var express = require('express');
-var app = express();
+var fs = require('fs');
 var path = require('path');
-var PORT = process.env.PORT || 5000
 var mongo = require('mongodb').MongoClient;
-/*var url = "mongodb://localhost:" + PORT + "/";*/
 
+/* variabili */
+var app = express();
+var database = "heroku_vbb83krx";
+var url = "mongodb://" + database + ":kf65ur2nkaqqqcluvtrnv56lkf@ds135776.mlab.com:35776/" + database;
+
+/* costanti */
+const PORT = process.env.PORT || 5000;
+
+/* path grafica */
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* path pagine */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+/* REST */
 app.get('/', (req, res) => res.render('pages/index'));
 
 /* Aggiunta dei dati della tabella */
 app.get('/document', function(req, res)
-{/*
-	mongo.connect(url, function(err, db)
+{
+	mongo.connect(url, { useNewUrlParser: true }, function(err, db)
 	{
 		if (err) throw err;
-		var cursor = db.db("mongodb-films");
-		var films = { name: "Company Inc", address: "Highway 37" };
-		cursor.collection("films").insertMany(myobj, function(err, res)
+
+		//console.log(path.join(__dirname, 'films/films.json'));
+
+		var films = JSON.parse(fs.readFileSync(path.join(__dirname, 'films/films.json'), 'utf8'));
+		var cursor = db.db(database);
+
+		cursor.collection("films").insertMany(films, function(err, res)
 		{
 			if (err) throw err;
-			console.log("1 document inserted");
+			console.log("film inseriti nel documento");
 			db.close();
 		});
-	});*/
-	res.render('pages/index');
+
+		db.close();
+	});
+	res.redirect('/');
 });
 
 /* Reset dei dati della tabella */
 app.get('/truncate', function(req, res)
 {
-	/*
-	mongo.connect(url, function(err, db)
+	mongo.connect(url, { useNewUrlParser: true }, function(err, db)
 	{
 		if (err) throw err;
-		var cursor = db.db("mongodb-films");
-		var films = { name: "Company Inc", address: "Highway 37" };
-		cursor.collection("films").insertMany(myobj, function(err, res)
+
+		var cursor = db.db(database);
+
+		cursor.collection("films").drop(function(err, res)
 		{
 			if (err) throw err;
-			console.log("1 document inserted");
-			db.close();
+
+			if (res)
+			{
+				console.log("Collection deleted");
+			}
+			else
+			{
+				console.log("Collection not deleted");
+			}
 		});
-	});*/
-	res.render('pages/index');
+
+		db.close();
+	});
+	res.redirect('/');
 });
 
+/* ascolto sulla porta 5000 */
 app.listen(PORT, () => console.log(`Listening on PORT: ${ PORT }`));
-/*
-mongo.connect(url, function(err, db)
+
+/* connessione al database */
+mongo.connect(url, { useNewUrlParser: true }, function(err, db)
 {
 	if (err) throw err;
-	var cursor = db.db("mongodb-films");
+	/*var cursor = db.db(database);
 	var myobj = { name: "Company Inc", address: "Highway 37" };
 	cursor.collection("films").insertMany(myobj, function(err, res)
 	{
 		if (err) throw err;
 		console.log("1 document inserted");
 		db.close();
-	});
-});*/
+	});*/
+	//console.log("un documento inserito");
+	db.close();
+});
